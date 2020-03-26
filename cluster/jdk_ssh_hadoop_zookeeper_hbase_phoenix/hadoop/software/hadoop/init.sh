08ad4fe2301f5a5b7ef16900e179535cac33b891
小桀 <812797569@qq.com>
2020-03-26 18:10:41 +0800

    调整hadoop版本为2.9.2

--- cluster/jdk_ssh_hadoop_zookeeper_hbase_phoenix/hadoop/software/hadoop/init.sh
+++ cluster/jdk_ssh_hadoop_zookeeper_hbase_phoenix/hadoop/software/hadoop/init.sh
@@ -2,7 +2,9 @@ mkdir -p ~/software/hadoop
 
 # 解压文件并将配置文件拷贝到指定目录
 # tar -zxvf /vagrant/software/hadoop/hadoop-3.2.1.tar.gz -C ~/software/hadoop --strip-components 1
-tar -zxvf /vagrant/software/hadoop/hadoop-2.8.5.tar.gz -C ~/software/hadoop --strip-components 1
+# tar -zxvf /vagrant/software/hadoop/hadoop-2.8.5.tar.gz -C ~/software/hadoop --strip-components 1
+tar -zxvf /vagrant/software/hadoop/hadoop-2.9.2.tar.gz -C ~/software/hadoop --strip-components 1
+
 
 # 拷贝配置文件到指定目录
 cp /vagrant/software/hadoop/conf/* ~/software/hadoop/etc/hadoop/
