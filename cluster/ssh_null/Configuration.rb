39dc7c6b967dced467d54d880e6d02ad99bfe473
吴俊杰 <wujj@4dstc.com>
2021-03-16 20:32:48 +0800

    auto commit

---
+++ cluster/ssh_null/Configuration.rb
@@ -0,0 +1,35 @@
+class Cluster
+    @@configures = Hash[
+        "toy" => Configure.new(
+            vmCounts = 3,
+            vmNamePrefixe = "toy",
+            hostnamePrefix = "toy",
+            vagrantTipPrefix = "toy_",
+            ipPrefix = "172.17.30",
+            ipStart = 100,
+            memorySize = 2048),
+    ]
+
+    def self.getConfiguration(projectName)
+        @@configures[projectName]
+    end
+
+    def self.getHosts(hostnamePrefix, idx)
+        hosts = "127.0.0.1\tlocalhost\n"
+        @@configures.each_value do |conf|
+            for i in 1..conf.vmCounts
+                hosts += "#{conf.ipPrefix}.#{conf.ipStart + i}\t#{conf.hostnamePrefix}#{i}\n"
+            end
+        end
+        hosts
+    end
+
+    def self.getHostnames(projectName)
+        conf = @@configures[projectName]
+        hostnames = ""
+        for i in 1..conf.vmCounts
+            hostnames += "#{conf.hostnamePrefix}#{i}\n"
+        end
+        hostnames
+    end
+end
